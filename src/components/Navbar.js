34c7de68f42e48d97eca2bd985b1fd849e739386
Inferno-SS5 <sumitsaha.ps@gmail.com>
2025-02-03 01:37:27 +0530

    Update name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar() {
   return (
     <div><nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
-        <Link className="navbar-brand fs-1 fst-italic" to="/">Gofood</Link>
+        <Link className="navbar-brand fs-1 fst-italic" to="/">Zestygo</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
